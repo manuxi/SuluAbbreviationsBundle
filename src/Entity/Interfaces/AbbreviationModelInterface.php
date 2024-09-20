cf2cb7005f679310d542678aece39f99eabcb051
Manuel Bertrams <mail@bertrams-media.de>
2024-09-21 00:54:47 +0200

    changed structure: ArchiveController doesn`t store/delete routes anymore. This is done now by ArchiveModel. Reason: have to explicitely call flush after routes are persisted.

--- src/Entity/Interfaces/AbbreviationModelInterface.php
+++ src/Entity/Interfaces/AbbreviationModelInterface.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 interface AbbreviationModelInterface
 {
     public function get(int $id, Request $request = null): Abbreviation;
-    public function delete(int $id, string $title): void;
+    public function delete(Abbreviation $entity): void;
     public function create(Request $request): Abbreviation;
     public function update(int $id, Request $request): Abbreviation;
     public function publish(int $id, Request $request): Abbreviation;
