0d70875e82ee46088aa13a510c3d466464ce7f85
Manuel Bertrams <manuel@bertrams-media.de>
2025-10-17 01:10:28 +0200

    AbbreviationExcerptModel

--- src/Entity/Models/AbbreviationExcerptModel.php
+++ src/Entity/Models/AbbreviationExcerptModel.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Manuxi\SuluAbbreviationsBundle\Entity\Models;
 
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
 use Manuxi\SuluAbbreviationsBundle\Entity\AbbreviationExcerpt;
 use Manuxi\SuluAbbreviationsBundle\Entity\Interfaces\AbbreviationExcerptModelInterface;
-use Manuxi\SuluAbbreviationsBundle\Entity\Traits\ArrayPropertyTrait;
 use Manuxi\SuluAbbreviationsBundle\Repository\AbbreviationExcerptRepository;
+use Manuxi\SuluSharedToolsBundle\Entity\Traits\ArrayPropertyTrait;
 use Sulu\Bundle\CategoryBundle\Category\CategoryManagerInterface;
 use Sulu\Bundle\MediaBundle\Entity\MediaRepositoryInterface;
 use Sulu\Bundle\TagBundle\Tag\TagManagerInterface;
@@ -29,7 +27,7 @@ class AbbreviationExcerptModel implements AbbreviationExcerptModelInterface
         AbbreviationExcerptRepository $abbreviationExcerptRepository,
         CategoryManagerInterface $categoryManager,
         TagManagerInterface $tagManager,
-        MediaRepositoryInterface $mediaRepository
+        MediaRepositoryInterface $mediaRepository,
     ) {
         $this->abbreviationExcerptRepository = $abbreviationExcerptRepository;
         $this->categoryManager = $categoryManager;
@@ -43,6 +41,7 @@ class AbbreviationExcerptModel implements AbbreviationExcerptModelInterface
     public function updateAbbreviationExcerpt(AbbreviationExcerpt $abbreviationExcerpt, Request $request): AbbreviationExcerpt
     {
         $abbreviationExcerpt = $this->mapDataToAbbreviationExcerpt($abbreviationExcerpt, $request->request->all()['ext']['excerpt']);
+
         return $this->abbreviationExcerptRepository->save($abbreviationExcerpt);
     }
 
@@ -75,7 +74,7 @@ class AbbreviationExcerptModel implements AbbreviationExcerptModelInterface
         if ($categoryIds && is_array($categoryIds)) {
             $abbreviationExcerpt->removeCategories();
             $categories = $this->categoryManager->findByIds($categoryIds);
-            foreach($categories as $category) {
+            foreach ($categories as $category) {
                 $abbreviationExcerpt->addCategory($category);
             }
         }
@@ -83,7 +82,7 @@ class AbbreviationExcerptModel implements AbbreviationExcerptModelInterface
         $tags = $this->getProperty($data, 'tags');
         if ($tags && is_array($tags)) {
             $abbreviationExcerpt->removeTags();
-            foreach($tags as $tagName) {
+            foreach ($tags as $tagName) {
                 $abbreviationExcerpt->addTag($this->tagManager->findOrCreateByName($tagName));
             }
         }
@@ -91,8 +90,8 @@ class AbbreviationExcerptModel implements AbbreviationExcerptModelInterface
         $iconIds = $this->getPropertyMulti($data, ['icon', 'ids']);
         if ($iconIds && is_array($iconIds)) {
             $abbreviationExcerpt->removeIcons();
-            foreach($iconIds as $iconId) {
-                $icon = $this->mediaRepository->findMediaById((int)$iconId);
+            foreach ($iconIds as $iconId) {
+                $icon = $this->mediaRepository->findMediaById((int) $iconId);
                 if (!$icon) {
                     throw new EntityNotFoundException($this->mediaRepository->getClassName(), $iconId);
                 }
@@ -103,8 +102,8 @@ class AbbreviationExcerptModel implements AbbreviationExcerptModelInterface
         $imageIds = $this->getPropertyMulti($data, ['images', 'ids']);
         if ($imageIds && is_array($imageIds)) {
             $abbreviationExcerpt->removeImages();
-            foreach($imageIds as $imageId) {
-                $image = $this->mediaRepository->findMediaById((int)$imageId);
+            foreach ($imageIds as $imageId) {
+                $image = $this->mediaRepository->findMediaById((int) $imageId);
                 if (!$image) {
                     throw new EntityNotFoundException($this->mediaRepository->getClassName(), $imageId);
                 }
